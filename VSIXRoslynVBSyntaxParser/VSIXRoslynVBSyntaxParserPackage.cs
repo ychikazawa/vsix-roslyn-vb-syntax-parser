7a62d94b72ee84cbbecb8b22259fd4f817b3cbb7
Yuto Chikazawa <chikazawa0517@gmail.com>
2021-07-13 19:15:07 +0900

    Add default command

--- VSIXRoslynVBSyntaxParser/VSIXRoslynVBSyntaxParserPackage.cs
+++ VSIXRoslynVBSyntaxParser/VSIXRoslynVBSyntaxParserPackage.cs
@@ -25,6 +25,7 @@ namespace VSIXRoslynVBSyntaxParser
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(VSIXRoslynVBSyntaxParserPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class VSIXRoslynVBSyntaxParserPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace VSIXRoslynVBSyntaxParser
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ParseSyntaxTreeCommand.InitializeAsync(this);
         }
 
         #endregion
